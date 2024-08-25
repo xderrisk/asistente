4a990665a450a33e87c09c1f94199c37c71ca4de
Sergio Galarza <xderrisk@gmail.com>
2024-08-25 18:18:40 -0500

    ruta de voz corregida

--- src/voz.py
+++ src/voz.py
@@ -8,7 +8,7 @@ class TextoAVoz:
     def __init__(self, texto):
         texto = texto
         tts = gTTS(text=texto, lang='es')
-        ruta_voz = ruta('media/grabacion.wav')
+        ruta_voz = ruta('media/voz.mp3')
         tts.save(ruta_voz)
 
         sistema = platform.system()
